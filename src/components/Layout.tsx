9e8cd56a092a4a3a8d848d03bd9f1c8b7c833ae3
maeda <takanori@stroly.jp>
2020-09-15 14:12:29 +0900

    op8: favicon対応

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,6 +13,7 @@ const Layout: React.FC<Props> = ({ children, title = 'This is the default title'
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
     </Head>
     <header>
       <nav>
